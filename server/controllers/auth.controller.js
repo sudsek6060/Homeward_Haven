07731aeb9f103fd4598cecfc5782ecf4bf526341
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2024-02-12 08:25:33 +0530

    sign-in functinality added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,28 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created sucessfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return next(errorHandler(404, "user not found"));
+    const userPassword = bcryptjs.compareSync(password, user.password);
+    if (!userPassword)
+      return next(errorHandler(401, "Please provide valid email or password"));
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
